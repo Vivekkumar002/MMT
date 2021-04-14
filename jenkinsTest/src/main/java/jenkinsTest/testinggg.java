533fdd715332d53d07c039c4fe787d87496c662c
unknown <vivekgupta@qainfotech.com>
2021-04-14 13:55:04 +0530

    jenkins

---
+++ jenkinsTest/src/main/java/jenkinsTest/testinggg.java
@@ -0,0 +1,42 @@
+package jenkinsTest;
+
+//import org.testng.annotations.Test;
+
+public class testinggg {
+
+//	@Test(groups = "pre-tests")
+//	public void init() {
+//		System.out.println("init resources");
+//	}
+//
+//	@Test(groups = "tests", dependsOnGroups = "pre-tests")
+//	public void insert() {
+//		System.out.println("inserting demo data");
+//	}
+//
+//	@Test(dependsOnMethods = "insert", groups = "tests")
+//	public void select() {
+//		System.out.println("selecting demo data");
+//	}
+//
+//	@Test(dependsOnMethods = "select", groups = "tests")
+//	public void update() {
+//		System.out.println("updating demo data");
+//	}
+//
+//	@Test(dependsOnMethods = { "insert", "update" }, groups = "tests")
+//	public void delete() {
+//		System.out.println("deleting demo data");
+//	}
+//
+//	@Test(dependsOnGroups = "tests")
+//	public void cleanup() {
+//		System.out.println("closing resources");
+//	}
+	
+	public static void main(String args[]) {
+		System.out.println("testing");
+	}
+	
+	
+}
